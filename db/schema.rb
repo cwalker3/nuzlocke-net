2393a125a108d2823d0db89ea7e0061b61dae8a8
Cory Walker <60561946+cwalker3@users.noreply.github.com>
2025-03-25 10:29:30 -0700

    create Game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_070943) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_25_172917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_games_on_title", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
